bfb7f6438b07b6f5ef082c2eb247cf4ae1777247
Dorai <hdorairaj@msn.com>
2022-03-27 16:13:24 +0530

    commit 5

---
+++ deps.js
@@ -0,0 +1,5 @@
+export { format } from "https://deno.land/std@0.103.0/datetime/mod.ts";
+export { createHash } from "https://deno.land/std@0.103.0/hash/mod.ts";
+export { Logger } from "https://deno.land/std@0.103.0/log/mod.ts";
+export { Client } from "https://deno.land/x/mysql@v2.10.1/mod.ts";
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
