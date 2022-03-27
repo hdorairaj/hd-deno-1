332f000337293253f3e1a570ef8ff11bafc5aa73
Dorai <hdorairaj@msn.com>
2022-03-27 10:33:15 +0530

    commit 3

--- hd-1/index.js
+++ hd-1/index.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 
 serve((_req) => {
-  return new Response("Hello World!", {
+  return new Response("Hello World123!", {
     headers: { "content-type": "text/plain" },
   });
 });
